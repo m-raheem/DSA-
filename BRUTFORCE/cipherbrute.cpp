d120a0bb16ace7ac3bcefff852e58978ddeb3edb
Muhammad Raheem Arif <raheemarif710@gmail.com>
2022-06-19 16:59:03 +0500

    Logical error resolved

--- BRUTFORCE/cipherbrute.cpp
+++ BRUTFORCE/cipherbrute.cpp
@@ -6,6 +6,10 @@ void cipher_bruteforce(string s){
     char ch;
     for(int j = 0;j<26;j++){
     for (int i = 0;i<s.length();i++){
+        if(j==0){
+            cout<<s[i];
+            continue;
+        }
         if (s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9'){
             cout<<s[i];
             continue;
@@ -22,11 +26,8 @@ void cipher_bruteforce(string s){
         cout<<s[i];
     }
     cout<<" "<<j<<endl;
-    
-
     }
 }
 int main(){
     cipher_bruteforce("TRVJRI TZGYVIJ RIV HLZKV VRJP KF TIRTB ");
-   
 }
